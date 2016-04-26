85b86df4780f386cf5c57a0e561537e9af287b29
annaflynn <anna.flynn@inin.com>
2016-04-26 15:17:42 -0400

    non handlebars dependent

--- modules/main.js
+++ modules/main.js
@@ -1,25 +1,13 @@
 import { TestSuite } from './utils/TestSuite';
 import { AudioTest, VideoTest, ConnectivityTest, AdvancedCameraTest, ThroughputTest, VideoBandwidthTest } from './utils/tests/defaultTests';
 
-var checkingMicrophone = true;
-var checkMicrophoneSuccess = false;
-var checkingCamera = true;
-var checkCameraSuccess = false;
-var checkingCameraAdvanced = true;
-var checkCameraAdvancedSuccess = false;
-var checkingConnectivity = true;
-var checkConnectivitySuccess = false;
-var checkingThroughput = true;
-var checkingThroughputSuccess = false;
-var checkingBandwidth = true;
-var checkingBandwidthSuccess = false;
-
 var troubleshootingLog = null;
 
 var video = true;
 var audio = true;
 
 var iceServers = null;
+var testSuite = new TestSuite();
 
 function init () {
   troubleshootingLog = [];
@@ -37,81 +25,50 @@ function startTroubleshooter () {
   };
   var mediaOptions = mediaOptions || { audio: true, video: true };
 
-  var testSuite = new TestSuite();
-  //
-  // if (audio) {
-  //   var audioTest = new AudioTest(mediaOptions, (err, logs) => {
-  //     setProperties({
-  //       checkingMicrophone: false,
-  //       checkMicrophoneSuccess: !err,
-  //       checkingVolume: false,
-  //       checkVolumeSuccess: !err
-  //     });
-  //     troubleshootingLog.push(logs);
-  //   });
-  //
-  //   testSuite.addTest(audioTest);
-  // }
+  if (audio) {
+    var audioTest = new AudioTest(mediaOptions, (err, logs) => {
+      // troubleshootingLog.push(logs);
+    });
+
+    testSuite.addTest(audioTest);
+  }
 
   if (video) {
-    // var videoTest = new VideoTest(mediaOptions, (err, logs) => {
-    //   setProperties({
-    //     checkingCamera: false,
-    //     checkCameraSuccess: !err
-    //   });
-    //   troubleshootingLog.push(logs);
-    // });
-    //
-    // var advancedCameraTest = new AdvancedCameraTest(mediaOptions, (err, logs) => {
-    //   setProperties({
-    //     checkingCameraAdvanced: false,
-    //     checkCameraAdvancedSuccess: !err
-    //   });
-    //   troubleshootingLog.push(logs);
-    // });
-
-    var bandwidthTest = new VideoBandwidthTest({iceConfig, mediaOptions}, (err, logs) => {
-      setProperties({
-        checkingBandwidth: false,
-        checkBandwidthSuccess: !err
-      });
-      troubleshootingLog.push(logs);
+    var videoTest = new VideoTest(mediaOptions, (err, logs) => {
+      // troubleshootingLog.push(logs);
     });
-    //
-    // testSuite.addTest(videoTest);
-    // testSuite.addTest(advancedCameraTest);
+
+    var advancedCameraTest = new AdvancedCameraTest(mediaOptions, (err, logs) => {
+      // troubleshootingLog.push(logs);
+    });
+
+    var bandwidthTest = new VideoBandwidthTest({iceConfig, mediaOptions}, (err, log) => {
+      // troubleshootingLog.push(logs);
+    });
+
+    testSuite.addTest(videoTest);
+    testSuite.addTest(advancedCameraTest);
     testSuite.addTest(bandwidthTest);
   }
-  //
-  // if (window.RTCPeerConnection) {
-  //   var connectivityTest = new ConnectivityTest(iceConfig, (err, logs) => {
-  //     setProperties({
-  //       checkingConnectivity: false,
-  //       checkConnectivitySuccess: !err
-  //     });
-  //     troubleshootingLog.push(logs);
-  //   });
-  //
-  //   var throughputTest = new ThroughputTest(iceConfig, (err, logs) => {
-  //     setProperties({
-  //       checkingThroughput: false,
-  //       checkThroughputSuccess: !err
-  //     });
-  //     troubleshootingLog.push(logs);
-  //   });
-  //
-  //   testSuite.addTest(connectivityTest);
-  //   testSuite.addTest(throughputTest);
-  // }
-  //
+
+  if (window.RTCPeerConnection) {
+    var connectivityTest = new ConnectivityTest(iceConfig, (err, logs) => {
+      // troubleshootingLog.push(logs);
+    });
+
+    var throughputTest = new ThroughputTest(iceConfig, (err, logs) => {
+      // troubleshootingLog.push(logs);
+    });
+
+    testSuite.addTest(connectivityTest);
+    testSuite.addTest(throughputTest);
+  }
+
   testSuite.runNextTest(troubleshootingLog);
-  //
-  // testSuite = testSuite;
 }
 
 function willDestroyElement () {
   try {
-    var testSuite = testSuite;
     if (testSuite && testSuite.running) {
       testSuite.stopAllTests();
     }
