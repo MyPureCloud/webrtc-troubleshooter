08b37a5baedd1d0feb9ed1045031940f23e162b4
CBrewski <iamsparticus33@gmail.com>
2018-07-16 08:30:32 -0600

    Broke out resolution tests into their own units

--- src/diagnostics/AdvancedCameraTest.js
+++ src/diagnostics/AdvancedCameraTest.js
@@ -11,12 +11,20 @@ export default class AdvancedCameraTest extends TestSuite {
     });
     this.stopOnFailure = true;
 
+    this.addTest(new CameraResolutionTest([[160, 120]], options));
+    this.addTest(new CameraResolutionTest([[320, 180]], options));
     this.addTest(new CameraResolutionTest([[320, 240]], options));
+    this.addTest(new CameraResolutionTest([[640, 360]], options));
     this.addTest(new CameraResolutionTest([[640, 480]], options));
+    this.addTest(new CameraResolutionTest([[768, 576]], options));
+    this.addTest(new CameraResolutionTest([[1024, 576]], options));
     this.addTest(new CameraResolutionTest([[1280, 720]], options));
-    this.addTest(new CameraResolutionTest([[160, 120], [320, 180], [320, 240], [640, 360], [640, 480], [768, 576],
-      [1024, 576], [1280, 720], [1280, 768], [1280, 800], [1920, 1080],
-      [1920, 1200], [3840, 2160], [4096, 2160]], options));
+    this.addTest(new CameraResolutionTest([[1280, 768]], options));
+    this.addTest(new CameraResolutionTest([[1280, 800]], options));
+    this.addTest(new CameraResolutionTest([[1920, 1080]], options));
+    this.addTest(new CameraResolutionTest([[1920, 1200]], options));
+    this.addTest(new CameraResolutionTest([[3840, 2160]], options));
+    this.addTest(new CameraResolutionTest([[4096, 2160]], options));
   }
   start () {
     super.start().then((results) => {
